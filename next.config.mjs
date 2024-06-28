b8149f716edfaebf408d090e170311122e8120c9
proshenjit <proshenjit@orangetoolz.com>
2024-06-29 01:55:14 +0600

    remove unnecessary code from the config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: "/public-view",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "s3.amazonaws.com",
-        port: "",
-        pathname: "/slf-media/**",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
