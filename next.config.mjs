eee48713b2009642f4afc57be76fa8334e067749
proshenjit <proshenjit@orangetoolz.com>
2024-06-29 01:24:31 +0600

    calendar app

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/public-view",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3.amazonaws.com",
+        port: "",
+        pathname: "/slf-media/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
